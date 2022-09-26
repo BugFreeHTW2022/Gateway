5a4ac3b1cddec02f64494da44deabb0c380317f7
mdvoryad <99253433+mdvoryad@users.noreply.github.com>
2022-09-26 14:51:43 +0200

    Swagger Docs

---
+++ src/main/java/htw/newProduct.java
@@ -0,0 +1,28 @@
+package htw;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class newProduct {
+    String name;
+    String size;
+    String teig;
+    String fill;
+    String glasur;
+    String description;
+    String extras;
+    String userName;
+
+
+    String convertToMessage(){
+        String message = this.name+","+this.size+","+this.teig+","+this.fill+","+this.glasur+","+this.description+","+this.extras+","+this.userName;
+        return message;
+    }
+}
+
+
